0dedf4d2303b5517627bcf5e9eee7f34285eeee5
Tomas Ave <cod3hulk@gmail.com>
2016-10-11 15:17:25 +0200

    Adjusted script

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ const alfredNotifier = require('alfred-notifier');
 
 alfredNotifier();
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const host = alfy.config.get('bamboo.host') || 'localhost'
+const port = alfy.config.get('bamboo.port') || 8080;
+const user = alfy.config.get('bamboo.user') || 'admin';
+const password = alfy.config.get('bamboo.password') || 'admin';
+const auth = new Buffer(`${user}:${password}`).toString('base64')
+
+const options = {
+  method: 'GET',
+  query: {
+    query: alfy.input
+  },
+  headers: {
+    'Authorization': 'Basic ' + auth
+  },
+  maxAge: 300000
+};
+
+alfy.fetch(`${host}:${port}/rest/api/latest/plan?max-result=1000`, options).then(data => {
+  const items = data.plans.plan
+    .map(x => ({
+      title: x.name,
+      arg: `${host}:${port}/browse/${x.key}`
+    }));
+
+  alfy.output(items);
+});
