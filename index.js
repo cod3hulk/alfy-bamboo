e8ff42e5ba60df85a39aa68af72c6e6134bff1e7
Tomas Ave <cod3hulk@gmail.com>
2016-10-18 21:51:22 +0200

    Support for multiple build servers

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ const alfy = require('alfy');
 const alfredNotifier = require('alfred-notifier');
 const fuzzy = require('fuzzy');
 const fs = require('fs');
+const rp = require('request-promise');
+const Promise = require('bluebird');
+const url = require('url');
 
 alfredNotifier();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const home = process.env.HOME || process.env.USERPROFILE;
 const configFile = process.argv[3] || `${home}/.alfy-bamboo.json`
-const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
-const host = config.host || 'localhost'
-const user = config.user || 'admin';
-const password = config.password || 'admin';
-const auth = new Buffer(`${user}:${password}`).toString('base64')
+const configs = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 
-const options = {
-  method: 'GET',
-  query: {
-    'query': alfy.input,
-    'max-results': 1000
-  },
-  headers: {
-    'Authorization': 'Basic ' + auth
-  },
-  maxAge: 300000
-};
+const options = configs.map(function(config) {
+  const auth = new Buffer(`${config.user}:${config.password}`).toString('base64')
+  return {
+    uri: `${config.host}/rest/api/latest/plan`,
+    headers: {
+      'Authorization': `Basic ${auth}`
+    },
+    qs: {
+      'query': alfy.input,
+      'max-results': 1000
+    },
+    json: true
+  }
+});
 
 const fuzzy_options = {
   extract: function(e) {
@@ -33,12 +35,18 @@ const fuzzy_options = {
   }
 };
 
-alfy.fetch(`${host}/rest/api/latest/plan`, options).then(data => {
-  const plans = fuzzy.filter(alfy.input, data.plans.plan, fuzzy_options);
-  const items= plans.map(x => ({
-    title: x.original.name,
-    subtitle: x.original.shortName,
-    arg: `${host}/browse/${x.original.key}`
+Promise.map(options, function(option) {
+  return rp(option)
+    .then(function(data) {
+      return fuzzy.filter(alfy.input, data.plans.plan, fuzzy_options);
+    });
+}).then(function(plans) {
+  const items = plans.reduce(function(a,b) {
+    return a.concat(b);
+  }).map(plan => ({
+    title: plan.original.name,
+    subtitle: plan.original.shortName,
+    arg: `https:\/\/${url.parse(plan.original.link.href).hostname}/browse/${plan.original.key}`
   }));
 
   alfy.output(items);
