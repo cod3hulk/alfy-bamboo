3906ad0514ae45a08d113de93a7b68f7ac7a3f72
Tomas Ave <t.ave@mytaxi.com>
2019-09-04 11:42:38 +0200

    UPdate bamboo workflow

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const alfy = require('alfy');
 const alfredNotifier = require('alfred-notifier');
 const fuzzy = require('fuzzy');
-const fs = require('fs');
-const rp = require('request-promise');
-const Promise = require('bluebird');
 const url = require('url');
+const fs = require('fs');
+const axios = require('axios');
 
 alfredNotifier();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
@@ -14,40 +13,38 @@ const home = process.env.HOME || process.env.USERPROFILE;
 const configFile = process.argv[3] || `${home}/.alfy-bamboo.json`
 const configs = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 
-const options = configs.map(function(config) {
-  const auth = new Buffer(`${config.user}:${config.password}`).toString('base64')
-  return {
-    uri: `${config.host}/rest/api/latest/plan`,
-    headers: {
-      'Authorization': `Basic ${auth}`
-    },
-    qs: {
-      'query': alfy.input,
-      'max-results': 1000
-    },
-    json: true
-  }
-});
-
 const fuzzy_options = {
   extract: function(e) {
     return e.name;
   }
 };
+const config = {
+  auth: {
+    username: configs.user,
+    password: configs.password
+  },
+  params: {
+    'max-results': 1000,
+    json: true
+  }
+}
 
-Promise.map(options, function(option) {
-  return rp(option)
-    .then(function(data) {
-      return fuzzy.filter(alfy.input, data.plans.plan, fuzzy_options);
-    });
-}).then(function(plans) {
-  const items = plans.reduce(function(a,b) {
-    return a.concat(b);
-  }).map(plan => ({
-    title: plan.original.name,
-    subtitle: plan.original.shortName,
-    arg: `https:\/\/${url.parse(plan.original.link.href).hostname}/browse/${plan.original.key}`
-  }));
+axios.get(
+  `${configs.host}/rest/api/latest/plan`,
+  config
+).then(res => {
+  return res.data.plans.plan
+}).then(plans => {
+  return fuzzy.filter(alfy.input, plans, fuzzy_options)
+}).then((filteredPlans) => {
+  return filteredPlans.map(plan => {
+    return {
+      title: plan.original.name,
+      subtitle: plan.original.shortName,
+      arg: `https:\/\/${url.parse(plan.original.link.href).hostname}/browse/${plan.original.key}`
+    }
+  });
+}).then(res => {
+  alfy.output(res)
+})
 
-  alfy.output(items);
-});
